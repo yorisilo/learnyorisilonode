cd304c0436639c193dee9503ea1e656baa285269
yorisilo <yorisilo@gmail.com>
2016-06-14 02:25:58 +0900

    refactoring program5

--- program5.js
+++ program5.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 
-var file = process.argv[2];
-var ext = process.argv[3];
+var dir = process.argv[2];
+var ext = '.' + process.argv[3];
 
-fs.readdir(file, function(err, list) {
-  list.forEach(logArrayElm);
+fs.readdir(dir, function(err, files) {
+  files.forEach(logArrayfile);
 });
 
-function logArrayElm(elm, index, arr) {
-  if(path.extname(elm) == '.'+ext) {
-    console.log( elm );
+function logArrayfile(file) {
+  if(path.extname(file) == ext) {
+    console.log( file );
   }
 }
