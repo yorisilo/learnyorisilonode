70fa6c2946b8d67a81c3a62bc3bf81a86916261f
yorisilo <yorisilo@gmail.com>
2016-06-14 23:44:41 +0900

    solved problem6

--- program6.js
+++ program6.js
@@ -1,27 +1,13 @@
-var fs = require('fs');
-var path = require('path');
+var find = require('./program6module.js');
 
 var dir = process.argv[2];
 var ext = process.argv[3];
-
-var find = function(dir, ext, callback) {
-  fs.readdir(dir, function(err, list) {
-    var files = [];
-    if (err) return callback(err);
-
-    list.forEach(function(file) {
-      if (path.extname(file) == '.' + ext) {
-        files.push(file);
-      }
-    });
-    return callback(null, files);
-  });
-};
 
 // コールバック関数を使うことによりネストした関数内の値を取ってくることができる．
 // 限定継続の shift/reset 的に使える．
 
 var files = find(dir, ext, function(err, files) {
+  if(err) throw err;
   files.forEach(function(file) {
     console.log( file );
   });
